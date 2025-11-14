ffe2799050cbe1d5446f3bb27e08f0700305f0b4
joy-lty <zh-wang2006@outlook.com>
2025-11-14 21:35:39 +0800

    update

--- oj2.1.c
+++ oj2.1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#define maxh 25
-int arr[(1 << maxh)],p[(1 << maxh)];
-int n, result = 0;
+#define maxh 21
+int arr[(1 << maxh)];//树节点
+int n, result = -50;
 int main(){
     int h;
     scanf("%d", &h);
@@ -9,13 +9,14 @@ int main(){
     for(int i = 0; i < n; i++){
         scanf("%d", &arr[i]);
     }
-    for(int i = (1<<(h-1)-1);i > 0;i--){
-        if(arr[i] = -99) continue;
-        int left = (arr[(i << 1) + 1]==-99 || p[(i<<1)+1]<0) ? 0:p[(i << 1) + 1];
-        int right = (arr[(i << 1) + 2]==-99 || p[(i<<1)+1]<0) ? 0:p[(i << 1) + 2];
-        p[i] += (left > right) ? left : right;
-        int temp = left + right + arr[i];
-        if(temp > result) result = temp;
+    for(int i = n-1 ;i >= 0;i--){
+        //整体思路：第i个节点为最高点往左右延伸
+        if(arr[i] == -99) continue;
+        int left = (arr[(i << 1) + 1]==-99 || arr[(i<<1)+1]<0) ? 0:arr[(i << 1) + 1];
+        int right = (arr[(i << 1) + 2]==-99 || arr[(i<<1)+2]<0) ? 0:arr[(i << 1) + 2];//arr[1]的左右最佳
+        int sum = left + right + arr[i];//计算以i为最高点的最佳
+        if(sum > result) result = sum;
+        arr[i] = arr[i] + (left > right ? left : right);//更新arr[i]
     }
     printf("%d\n", result);
     return 0;
